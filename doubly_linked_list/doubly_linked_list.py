893f41fd9380b0ba0ad3f573eb53f0409bdccc55
[Brandon Teague] <[teaguebrandon@outlook.com]>
2020-07-15 14:49:47 -0400

    finished mvp

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -7,7 +7,16 @@ class ListNode:
         self.prev = prev
         self.value = value
         self.next = next
-            
+    def delete(self):
+        if self.prev:
+            if self.next:
+                self.prev.next = self.next
+                self.next.prev = self.prev
+            else:
+                self.prev.next = None
+        else:
+            if self.next:
+                self.next.prev = None
 """
 Our doubly-linked list class. It holds references to 
 the list's head and tail nodes.
@@ -16,67 +25,127 @@ class DoublyLinkedList:
     def __init__(self, node=None):
         self.head = node
         self.tail = node
-        self.length = 1 if node is not None else 0
-
+        if node == None:
+            self.length = 0
+        else:
+            self.length = 1
     def __len__(self):
         return self.length
-    
     """
     Wraps the given value in a ListNode and inserts it 
     as the new head of the list. Don't forget to handle 
     the old head node's previous pointer accordingly.
     """
     def add_to_head(self, value):
-        pass
-        
+        new_node = ListNode(value, None)
+        self.length += 1
+        if self.head:
+            head = self.head
+            self.head = new_node
+            head.prev = self.head
+            self.head.next = head
+        else:
+            self.head = new_node
+            self.tail = new_node
     """
     Removes the List's current head node, making the
     current head's next node the new head of the List.
     Returns the value of the removed Node.
     """
     def remove_from_head(self):
-        pass
-            
+        self.length -= 1
+        if self.head:
+            if self.head.next:
+                value = self.head.value
+                self.head = self.head.next
+                return value
+            else:
+                value = self.head.value
+                self.head = None
+                self.tail = None
+                return value
+        else:
+            return None
     """
     Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly.
     """
     def add_to_tail(self, value):
-        pass
-            
+        new_node = ListNode(value, None)
+        self.length += 1
+        if self.head:
+            tail = self.tail
+            self.tail = new_node
+            self.tail.prev = tail
+            tail.next = self.tail
+        else:
+            self.head = new_node
+            self.tail = new_node
     """
     Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node.
     """
     def remove_from_tail(self):
-        pass
-            
+        self.length -= 1
+        if not self.head:
+            return None
+        else:
+            if self.head is self.tail:
+                value = self.head.value
+                self.head = None
+                self.tail = None
+                return value
+            else:
+                self.tail = self.tail.prev
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List.
     """
     def move_to_front(self, node):
-        pass
-        
+        if node is self.head:
+            return
+        else:
+            value = node.value
+            self.delete(node)
+            self.add_to_head(value)
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List.
     """
     def move_to_end(self, node):
-        pass
-
+        if node is self.tail:
+            return
+        else:
+            value = node.value
+            self.delete(node)
+            self.add_to_tail(value)
     """
     Deletes the input node from the List, preserving the 
     order of the other elements of the List.
     """
     def delete(self, node):
-        pass
-
+        self.length -= 1
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+        elif self.head is node:
+            self.head = node.next
+            node.delete()
+        elif self.tail is node:
+            self.tail = node.prev
+            node.delete()
     """
     Finds and returns the maximum value of all the nodes 
     in the List.
     """
     def get_max(self):
-        pass
+        current = self.head
+        max_value = current.value
+        while current is not None:
+            if current.value > max_value:
+                max_value = current.value
+            current = current.next
+
+        return max_value
--- stack/stack.py
+++ stack/stack.py
@@ -10,29 +10,29 @@ return elements in Last In First Out order.
    implementing a Stack?
 """
 
-# import sys
-# sys.path.append('singly_linked_list')
-# from singly_linked_list import LinkedList
+import sys
+sys.path.append('singly_linked_list')
+from singly_linked_list import LinkedList
 
-# class Stack:
+class Stack:
 
-#     def __init__(self):
-#         self.size = 0
-#         self.storage = LinkedList()
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
 
-#     def __len__(self):
-#         return self.size
+    def __len__(self):
+        return self.size
 
-#     def push(self, value):
-#         self.size += 1
-#         self.storage.add_to_tail(value)
+    def push(self, value):
+        self.size += 1
+        self.storage.add_to_tail(value)
 
-#     def pop(self):
-#         if self.size > 0:
-#             self.size -= 1
-#             return self.storage.remove_tail()
-#         else:
-#             return None  # Nothing is there
+    def pop(self):
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_tail()
+        else:
+            return None  # Nothing is there
 
 # class Stack:
 
